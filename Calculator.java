3c66a7656191e7ee78109b54f7e1077809a7c399
vinod malode <malodevinod9108@gmail.com>
2018-12-17 15:22:39 +0530

    modified

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator
+{
+int x=10,y=20;
+	public void add()
+	{
+<<<<<<< HEAD
+	System.out.println(x+y);	
+	System.out.println(x+y);	
+	System.out.println(x+y);	
+	System.out.println(x+y);	
+
+=======
+	System.out.println(x+y);
+System.out.println(x+y);
+System.out.println(x+y);
+System.out.println(x+y);	
+>>>>>>> cal
+	}
+
+	public static void main(String args[])
+	{
+	Calculator c=new Calculator();
+	c.add();
+	}
+}
